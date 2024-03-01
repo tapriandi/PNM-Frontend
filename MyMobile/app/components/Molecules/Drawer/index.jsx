caf6675ed9efc862ae82ef53e08a5f6613e7f53c
tapriandi <tapriandi@gmail.com>
2024-03-01 15:47:36 +0700

    try add drawer

--- MyMobile/app/components/Molecules/Drawer/index.jsx
+++ MyMobile/app/components/Molecules/Drawer/index.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Button, Text } from 'react-native';
 import { Drawer } from 'react-native-drawer-layout';
 
-export default function DrawerExample() {
+export default function index() {
   const [open, setOpen] = React.useState(false);
 
   return (
